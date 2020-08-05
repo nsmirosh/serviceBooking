3dcc20ad008954ec2a60613c4a74828f8a020658
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2020-08-05 19:06:16 +0300

    Added some other stuff

---
+++ lib/branch.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+class Branch {
+  Branch({@required this.name, @required this.address});
+  String address;
+  String name;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,155 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
-import 'city.dart';
 
 void main() => runApp(MyApp());
 
+final dummySnapshot = [
+  {"name": "Filip", "votes": 15},
+  {"name": "Abraham", "votes": 14},
+  {"name": "Richard", "votes": 11},
+  {"name": "Ike", "votes": 10},
+  {"name": "Justin", "votes": 1},
+];
+
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Baby Names',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Baby Name Votes')),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    // TODO: get actual snapshot from Cloud Firestore
+    return _buildList(context, dummySnapshot);
+  }
+
+  Widget _buildList(BuildContext context, List<Map> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, Map data) {
+    final record = Record.fromMap(data);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.votes.toString()),
+          onTap: () => print(record),
+        ),
+      ),
+    );
+  }
+}
+
+class Record {
+  final String name;
+  final int votes;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['votes'] != null),
+        name = map['name'],
+        votes = map['votes'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$name:$votes>";
+}
+
+
+
+
+
+
+
+
+
+/*import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/material.dart';
+
+import 'branch_screen.dart';
+import 'city.dart';
+
+
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+   final FirebaseApp app = await FirebaseApp.configure(
+    name: 'db2',
+    options:
+      const FirebaseOptions(
+      googleAppID: '1:278139192337:android:32ce4faf2dfa27cf9e9db0',
+      apiKey: 'AIzaSyAA175svGza0vPrmm5QcGRppELCXGAjMZc',
+      databaseURL: 'https://servicebooking-4e109.firebaseio.com/',
+    ),
+  );
+
+  runApp(MyHomePage());
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({this.app});
+  final FirebaseApp app;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState  extends State<MyHomePage> {
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    *//*final FirebaseDatabase database = FirebaseDatabase(app: widget.app);
+    database.reference().child('balls').once().then((DataSnapshot snapshot) {
+      print('Connected to second database and read ${snapshot.value}');
+    });
+    database.setPersistenceEnabled(true);
+    database.setPersistenceCacheSizeBytes(10000000);*//*
+   *//* Future<List<QuerySnapshot>> stuff = Firestore.instance.collection('cities').snapshots().toList();
+    stuff.then((value) => print('value of the query stuff = $value'));*//*
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -41,12 +186,11 @@ Widget _myListView(BuildContext context) {
       return ListTile(
           title: Text(city.name),
           subtitle: Text('branches: ${city.branchAmount}'),
-          onTap: () => _doSomething());
+          onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondRoute()),
+              ));
     },
   );
-}
+}*/
 
-_doSomething() {
-
-
-}
