9c70ee6f945c9aa8f0572ba968369be70f9eb850
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2020-08-06 19:33:47 +0300

    Fetching the data and displaying the progress bar works ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+const CITIES_PATH = "cities";
 
-final dummySnapshot = [
-  {"name": "Filip", "votes": 15},
-  {"name": "Abraham", "votes": 14},
-  {"name": "Richard", "votes": 11},
-  {"name": "Ike", "votes": 10},
-  {"name": "Justin", "votes": 1},
-];
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -29,6 +23,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var data;
+
+  @override
+  void initState() {
+    super.initState();
+    getData().then((value) {
+      setState(() => data = value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,22 +42,26 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildBody(BuildContext context) {
-    // TODO: get actual snapshot from Cloud Firestore
-    return _buildList(context, dummySnapshot);
+    if (data != null) {
+      return _buildList(context, data);
+    } else {
+      return Center(child: CircularProgressIndicator());
+    }
   }
 
-  Widget _buildList(BuildContext context, List<Map> snapshot) {
+  Widget _buildList(BuildContext context, QuerySnapshot qShot) {
     return ListView(
       padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+      children:
+          qShot.documents.map((data) => _buildListItem(context, data)).toList(),
     );
   }
 
-  Widget _buildListItem(BuildContext context, Map data) {
-    final record = Record.fromMap(data);
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final city = City.fromMap(data.data);
 
     return Padding(
-      key: ValueKey(record.name),
+      key: ValueKey(city.name),
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
       child: Container(
         decoration: BoxDecoration(
@@ -61,136 +69,35 @@ class _MyHomePageState extends State<MyHomePage> {
           borderRadius: BorderRadius.circular(5.0),
         ),
         child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.votes.toString()),
-          onTap: () => print(record),
+          title: Text(city.name),
+          trailing: Text(city.numberOfBranches.toString()),
+          onTap: () => print(city),
         ),
       ),
     );
   }
 }
 
-class Record {
+class City {
   final String name;
-  final int votes;
+  final int numberOfBranches;
   final DocumentReference reference;
 
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
+  City.fromMap(Map<String, dynamic> map, {this.reference})
       : assert(map['name'] != null),
-        assert(map['votes'] != null),
+        assert(map['numberOfBranches'] != null),
         name = map['name'],
-        votes = map['votes'];
+        numberOfBranches = map['numberOfBranches'];
 
-  Record.fromSnapshot(DocumentSnapshot snapshot)
+  City.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data, reference: snapshot.reference);
 
   @override
-  String toString() => "Record<$name:$votes>";
+  String toString() => "Record<$name:$numberOfBranches>";
 }
 
-
-
-
-
-
-
-
-
-/*import 'dart:async';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_database/ui/firebase_animated_list.dart';
-import 'package:flutter/material.dart';
-
-import 'branch_screen.dart';
-import 'city.dart';
-
-
-Future<void> main() async {
-
-  WidgetsFlutterBinding.ensureInitialized();
-
-   final FirebaseApp app = await FirebaseApp.configure(
-    name: 'db2',
-    options:
-      const FirebaseOptions(
-      googleAppID: '1:278139192337:android:32ce4faf2dfa27cf9e9db0',
-      apiKey: 'AIzaSyAA175svGza0vPrmm5QcGRppELCXGAjMZc',
-      databaseURL: 'https://servicebooking-4e109.firebaseio.com/',
-    ),
-  );
-
-  runApp(MyHomePage());
+Future<QuerySnapshot> getData() async {
+  QuerySnapshot snapshot =
+      await Firestore.instance.collection(CITIES_PATH).getDocuments();
+  return snapshot;
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({this.app});
-  final FirebaseApp app;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState  extends State<MyHomePage> {
-
-
-  @override
-  void initState() {
-    super.initState();
-
-    *//*final FirebaseDatabase database = FirebaseDatabase(app: widget.app);
-    database.reference().child('balls').once().then((DataSnapshot snapshot) {
-      print('Connected to second database and read ${snapshot.value}');
-    });
-    database.setPersistenceEnabled(true);
-    database.setPersistenceCacheSizeBytes(10000000);*//*
-   *//* Future<List<QuerySnapshot>> stuff = Firestore.instance.collection('cities').snapshots().toList();
-    stuff.then((value) => print('value of the query stuff = $value'));*//*
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ListViews',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('ListViews')),
-        body: BodyLayout(),
-      ),
-    );
-  }
-}
-
-class BodyLayout extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return _myListView(context);
-  }
-}
-
-Widget _myListView(BuildContext context) {
-  final List<City> cities = [
-    City(name: 'Kyiv', branchAmount: 2),
-    City(name: 'Lviv', branchAmount: 5)
-  ];
-
-  return ListView.builder(
-    itemCount: cities.length,
-    itemBuilder: (context, index) {
-      var city = cities[index];
-      return ListTile(
-          title: Text(city.name),
-          subtitle: Text('branches: ${city.branchAmount}'),
-          onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SecondRoute()),
-              ));
-    },
-  );
-}*/
-
